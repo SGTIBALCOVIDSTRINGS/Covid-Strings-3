31506c3b4603ea7af95fede463d4f660b35bd14f
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-09 20:16:55 -0500

    Create Covid Strings 3 first round.js
    I will edit the parts I haven't gotten to later.

---
+++ Covid Strings 3 first round.js
@@ -0,0 +1,99 @@
+doc. type. HTML
+script
+html 
+head
+utf-eight
+viewport content=width=device-width
+intial-scale=one
+title covid strings3 first round bracket
+link rel = stylesheet href =styles.css
+head
+body
+filename=covidstrings3 first round .js
+createround
+round = document.createelement(div);
+round.classlist.add(covid strings3 first round);
+document addeventlistener(domcontentloaded)
+bracket=document getelementbyiD(bracket)
+creatematchup (teal versus pink)
+matchup= document.createelement(div)
+matchup.covid strings seed list .add(matchup)
+bracketAppend.child(matchup)
+bracket=document getelementbyid(bracket)
+matchup= document.createelement(div)
+create matchup (violet versus turquoise)
+matchup.covid strings seed list .add(matchup)
+bracket append.child(matchup)
+bracket=document getelementbyid(bracket)
+create matchup(yellow versus purple)
+matchup= document.createelement(div)
+matchup.covid strings seed list .add(matchup)
+bracket append.child(matchup)
+matchup= document.createelement(div)
+create matchup(orange versus black)
+matchup.covid strings seed list .add(matchup)
+bracket append.child(matchup)
+return round 
+teams=[One: Black Night Riders, Two Purple Brave Hearts Three: Pink Covid Disease Fighters Four:Turquoise Waves Five:Violet Relivers Six:Teal Terminators Seven:Yellow Moxie Eight:Orange RNA Messengers]
+Import Java Util.Scanner
+Import Java.Util
+Questions[]Questions={
+    New Question1
+    New Question2 
+    New Question3
+    New Question4
+    New Question5 
+    New Question6
+    New Question7
+    New Question8
+    New Question9 
+    New Question10
+    New Question11
+    New Question12
+    New Question13
+    New Question14 
+    New Question15 
+    New Question16 
+    New Question17 
+Collections.Shuffle(Arrays.Aslist(Questions)
+Private String GetBlanks(Int NumberofBlanks)
+Char[] Chars = New Char[NumberofBlanks]
+AdvancingTeams=CurrentTeams.Slice(Zero,CurrentTeams/Two)
+Bracket.AddEventListener(Click Eevent) => {
+    Declare Winner
+        If (Event.Target.Covid Strings Seed List .Contains(Team)) {
+            Const Winner = Event.Target.TextContent;
+            Let Result = DeclareWinner(Team1Score, Team2Score);
+            If Pink Score >=Fourty:
+            Console.Log(Pink Covid Disease Fighters Win)
+            Else:
+            Console.Log(Teal Terminators Win)
+Bracket.AddEventListener(Click (Event) => {
+        If (Event.Target.ClassList.Contains(Team)) {
+            Const Winner = Event.Target.TextContent;
+            Let Result= Declare Winner(Team1 Score, Team2 Score)
+            If Turquoise Score>=Fourty:
+            Console.Log(Turquoise Waves Win)
+            Else:
+            Console.Log(Violet Relivers Win)
+            Bracket.AddEventListener(Click, (Event) => {
+        If (Event.Target.ClassList.Contains(Team)) {
+            Const Winner = Event.Target.TextContent;
+            Let Result= Declare Winner(Team1 Score, Team2 Score)
+            If Purple Score >=Fourty:
+            Console.Log(Purple Brave Hearts Win)
+            Else:
+            Console.Log(Yellow Moxie Wins)
+            Bracket.AddEventListener(Click, (Event) => {
+        If (Event.Target.ClassList.Contains(Team)) {
+            Const Winner = Event.Target.TextContent;
+            Let Result=DeclareWinner(Team1 Score, Team2 Score)
+            If Black Score >=Fourty:
+            Console.Log(Black Night Riders Win)
+            Else:
+            Console.Log(Orange RNA Messengers Win)
+            Return FileName 
+            
+
+        
+
