fa076769b90b127a59fa47ec136448e198c54422
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-10 19:25:54 -0500

    Create Covid Strings 3 second round .js
    round two for version 3 luckily edited the part of having some html where it shouldn't have been.

---
+++ Covid Strings 3 second round .js
@@ -0,0 +1,58 @@
+doc type html>
+    <script>
+  html
+head
+utf-eight
+viewport content=width=device-width
+intial-scale=one
+title covid strings3 second round
+link rel = stylesheet href =styles.css
+head
+body
+filename=covid strings3 second round.js 
+highest remaining seed =(greatest remaining num value in covid strings list)
+lowest remaining seed = (lowest remaining num value in covid strings list)
+intermediate seed1=(first middle remaining value in covid strings list)
+Intermediate Seed2=(second middle remaining value in covid strings list)
+createround
+round = document.createelement(div);
+round.classlist.add(covid strings3 first round);
+document addeventlistener(domcontentloaded)
+bracket=document getelementbyiD(bracket)
+matchup=document.createelement(matchup)
+create matchup (intermediate seed2 versus intermediate seed1)
+matchup.covid strings seed list.add(matchup)
+bracket.appendchild(matchup)
+matchup=document.createelement(matchup)
+create matchup(lowest remaining seed versus highest remaining seed )
+matchup.covid strings seed list.add(matchup)
+bracket.appendchild(matchup)
+java util.scanner
+java.util
+questions[]questions={
+question18
+question19
+question20
+question21
+question22
+question23 
+question24
+question25
+question26
+question27 
+collections.shuffle(arrays.aslist(questions)
+private string getblanks(int numberofblanks)
+char[] chars = char[numberofblanks]
+arrays.fill chars 
+return string chars 
+advancingteams=slice(currentteams)
+bracket.innerHTML = html;
+result =declarewinner(team1 score, team2 score)
+if intermediateseed1score>=fourty:
+console.log(intermediate seed1 wins)
+result =declarewinner(team1score,team2score)
+if highestremainingseedscore>=fourty:
+console.log(highest remaining seed wins)
+else:
+console.log(lowest remaining seed wins)
+return filename 
