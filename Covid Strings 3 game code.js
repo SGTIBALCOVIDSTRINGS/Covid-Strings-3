aaf9640d6b000c77ef1fafb42dcdba2dd69f09d9
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-12 13:00:11 -0500

    Update Covid Strings 3 game code.js
    done with edits for game code. Questions in this version include a reference to a Christmas carol the master's and the 2021 summer Olympics.

--- Covid Strings 3 game code.js
+++ Covid Strings 3 game code.js
@@ -66,30 +66,30 @@ const entries object entries(obj);
 const value object= [team color]
 covid strings3 team list=[black,purple,pink,turquoise,violet,teal,yellow,orange]
 covid strings3 phrase list=[non-concatenated strings a mask is not the shape of an arrowhead,covid rarely wins a vote against vaccines,kelly clarkson is right; christmas isnt canceled Just Big Christmas Gatherings
-A Faucian Bargain Is Better Than A Faustian Bargain 
-The Masters In Fall Is Not The Same As The Fall Of The Masters
-Similarly, The Summer Olympics Doesnt Disappoint Because Of The Fact That Its Held In An Odd Year
- The UnXplained Should  Have A Covid-Ninteen Specific Special
-Its Pandemic Special Overall Is Still Good
-Concatenated Strings
-Pandemic+Time+Is+Somewhat+Different+Than+Normal+Time
-It+Is+More+Polychronic+Than+You+Think
-Covid+Cough+Cannot+Be+Cured+By+Soda+Alone
-Nothing+About+The+Pandemic+Resembles+The+Holocaust
-It+Is+Also+Inappropriate+To+Not+Vaccinate+The+World+As+Well
-If+You+Dont+You+May+Be+Visited+By+At+Least+Three+More+Variants
-Chinas+Covid+Victory+Musuem+Sounds+Like+North+Koreas+Korean+War+Museum
-There+Are+Plenty+Of+Chinese+Government+Covid+Lies]
-Covid Strings3 Multiple Choice List=[One: A Charging Buffalo B An Arrow C A Mask D Line With A Slope Of Two, Two: A Light Years B Not At All C Somewhat D Similar, Three: A Like Futurama B Polychronic C Monochronic D SNL Like
-,Four: A Influenza B Covid C RSV D SAD, Five: A Dodgeball B Vote C Referendum D Caucus, Six: A You B Trump C Big Gatherings D Bowling Alone, Seven: A Vaccine B Pandemic C Trumps Family Seperation Policy D Ukraine War,
- Eight: A Wise B Inappropriate C Just D Sensical, Nine: A World Chess Championship B Olympics C Masters D World Cup, Ten: A Four B At Least Three B Eight D Infintely,Eleven:A Lockdown B Trumpian C Faucian D Covid,
- Twelve: A The Netherlands B The Smithsonians C  Taiwans D North Koreas, Thirteen: A Winter Olympics B X Games C Summer Olmypics D Invictus Games, Fourteen: A Chinese  B Malaysian  C US D Spanish,
- Fifteen:A Black Death Thirteen Fourty Eight  B Dont Call Covid Kung Flu C Spanish Flu D Covid-Ninteen, Sixteen:A Government Overreach B Defense Against Japanese Killer Ghosts C Protection D Misinformation Campaign,
-Seventeen:A Natural Disaster B Miracle C Graveyard D Pandemic,Eighteen:A Negative Fifteen B Negative One Hundred C Zero D One Hundred Thousand Ninteen:A Trump News B Covid C Russian Interference In Americas Elections D Specific Variants,
-Twenty:A Yes B No.  
-Covid Strings3 Math Problems List=[If There Were Two Hundred Thousand Cases Reported In Wuhan On December Ninteenth And The Threshold For Ordering A Shutdown Is When The Number Of Cases Exceeds Fourty Thousand Would It Shut Down If A Decision Had To Be Made By December Twenty First?,
-If The Rate Was Five And Two Ninths Percent And The Cut For Deciding Whether To Shut Down Is Anything Greater Than Or Equal To Four And A Fifth Percent Should It Shut Down?, After Seven Weeks If The Rate Decreases To Two And Seven Ninths Percent Should The Lockdown Remain In Place?,
-if rates climb to four and a quarter percent should a lockdown be reinstated?,if the rate decreases to four and a fifth percent should the lockdown remain in place?,if oil prices are projected to decline for at least two years and the rate is approximately thirty dollars and seventy three cents per month how much could it decline two years from now?, if the price of oil fell below zero dollars in twenty-twenty and projections of demand coming back say it wont until at least late twenty-twenty one how much would it fall to if the estimated rate per month is five percent?, a+report+claims+disease+x+could+be+twenty+times+deadlier+than+covid+if+gdp+globally+decreased+by+three+percent+how+much+could+it+drop+when+disease+x+hits?,the+us+GDP+Dropped+THREE AND FOUR TENTHS PERCENT+Overall+Given+This+What+Could+It+Drop+To+When+Disease+X+Hits?, If+It+Turns+Out+That+The+Actual+Range+Is+Anywhere+From+Ten+To+Fifteen+Times+What+Would+The+Percent+Be?,On+The+Other+Hand+If+WHO+Projections+Say+That+The+Range+Is+More+Fifteen+To+Twenty+Times+For+Countries+That+Spend+Poorly+If+US+Spending+For+Disease+X+Was+Only+Ten+Percent+What+Is+The+Decrease+In+GDP?,If+It+Turns+Out+That+The+Disease+X+Was+Prevented+From+Being+A+Pandemic+In+The+First+Place+What+Is+The+Effect+On+GDP?,If+It+Ends+Up+Being+That+Disease+X+Was+A+False+Positive+And+There+Is+Nothing+To+Worry+About+Yet+Should+You+Sell+Any+Stock?]
+a faucian bargain is Better than a faustian bargain 
+the masters in fall is not the same as the fall of the masters
+similarly, the summer olympics doesnt disappoint because of the fact that its held in an odd year
+the unxplained should  have a covid-nineteen specific special
+its pandemic special overall is still good
+concatenated strings
+pandemic+time+is+somewhat+different+than+normal+time
+it+is+more+polychronic+than+you+think
+covid+cough+cannot+be+cured+by+soda+alone
+nothing+about+the+pandemic+resembles+the+holocaust
+it+is+also+inappropriate+to+not+vaccinate+the+world+as+well
+if+you+dont+you+may+be+visited+by+at+least+three+more+variants
+chinas+covid+victory+musuem+sounds+like+north+koreas+korean+war+museum
+there+are+plenty+of+chinese+government+covid+lies]
+covid strings3 multiple choice list=[1:a charging buffalo b an arrow c a mask d line with a slope of two, 2: a light years b not at all c somewhat d similar, 3: a like futurama b polychronic c monochronic d snl like
+,4: a influenza b covid c rsv d sad, 5: a dodgeball b vote c referendum d caucus,6: a you b trump c big gatherings d bowling alone, 7: a vaccine b pandemic c trumps family seperation policy d ukraine war,
+ 8: a wise b inappropriate c just d sensical, 9: a world chess championship b olympics c masters d world cup, 10: a four b at least three c eight d infintely,11:a lockdown b trumpian c faucian d covid,
+ 12: a the netherlands b the smithsonians c taiwans d north koreas, 13: a winter olympics b x games c summer olmypics d invictus games, 14: a chinese b malaysian c us d spanish,
+ 15:a black death thirteen fourty eight b dont call covid kung flu c spanish flu d covid-nineteen, 16:a government overreach b defense against japanese killer ghosts c protection d misinformation campaign,
+17:a natural disaster b miracle c graveyard d pandemic,18:a negative fifteen b negative one hundred c zero d one hundred thousand 19:a trump news b covid c russian interference in americas elections d specific variants,
+20:a yes b no.  
+covidstrings3 math problems list=[if there were two hundred thousand cases reported in wuhan on december ninteenth and the threshold for ordering a shutdown is when the number of cases exceeds fourty thousand would it shut down if a decision had to be made by december twenty first?,
+if the rate was five and two ninths percent and the cut for deciding whether to shut down is anything greater than or equal to four and a fifth percent should it shut down?, after seven weeks if the rate decreases to two and seven ninths percent should the lockdown remain in place?,
+if rates climb to four and a quarter percent should a lockdown be reinstated?,if the rate decreases to four and a fifth percent should the lockdown remain in place?,if oil prices are projected to decline for at least two years and the rate is approximately thirty dollars and seventy three cents per month how much could it decline two years from now?, if the price of oil fell below zero dollars in twenty-twenty and projections of demand coming back say it wont until at least late twenty-twenty one how much would it fall to if the estimated rate per month is five percent?, a+report+claims+disease+x+could+be+twenty+times+deadlier+than+covid+if+gdp+globally+decreased+by+three+percent+how+much+could+it+drop+when+disease+x+hits?,the+us+gdp+dropped+three and four tenths percent+overall+given+this+what+could+it+drop+to+when+disease+x+hits?,if+it+turns+out+that+the+actual+range+is+anywhere+from+ten+to+fifteen+times+what+would+the+percent+be?,on+the+other+hand+if+who+projections+say+that+the+range+is+more+fifteen+to+twenty+times+for+countries+that+spend+poorly+if+us+spending+for+disease+x+was+only+ten+percent+what+is+the+decrease+in+gdp?,if+it+turns+out+that+the+disease+x+was+prevented+from+being+a+pandemic+in+the+first+place+what+is+the+effect+on+gdp?,if+it+ends+up+being+that+disease+x+was+a+false+positive+and+there+is+nothing+to+worry+about+yet+should+you+sell+any+stock?]
 covid strings3 sentence list=[masks +are +part of a broader protection strategy against covid.,covid+string+games+are+a+new+way+to+help+archive+covid ]
 covid strings3 bonus question=(would+there+need+to+be+a+comma+if+the+previous+sentence+included+the+word+below?)
 string.split   
@@ -101,9 +101,9 @@ java.util
 questions[]questions={
 collections.shuffle(arrays.aslist(questions)
 private string getblanks(int numberofblanks)
-char[] chars = new char[numberofblanks]
+char[] chars =char[numberofblanks]
 arrays.fill chars 
-return new string chars 
+return string chars 
 if math problem== x percent:
 subtract exact from one hundred
 if math problem== margin of error:
@@ -111,7 +111,7 @@ add and subtract exact
 else:
 remain constant
 userin.select team
-import meta.
+importmeta.
 class myclass
 constructor (non-xoncatenated)
 return category
@@ -119,17 +119,17 @@ if category==math problem:
 type answer
 else:
 select from multiple choice
-const myclass=newmyclass(concatenated) 
+myclass=newmyclass(concatenated) 
 return category
 if category==math problem:
 type answer
 else:
 select from multiple choice
-import syntax 
+importsyntax 
 substring(start,end)
 userin.multiple choice
 if selectedanswer==correct answer:
-return true
+log true
 else:
-return false
+log false
 return filename
