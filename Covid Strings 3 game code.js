b90bcbf8b3ffb305d0a2713f4d2b16ef6b4a8a96
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-11 20:30:37 -0500

    Create Covid Strings 3 game code.js
    I know there are some more changes but I can revise them later.

---
+++ Covid Strings 3 game code.js
@@ -0,0 +1,135 @@
+doc type html
+script
+filename=covid strings3 .js
+java.awt graphics 
+this.img=document.createelement(covid)
+img.src=http://www.google.com/intl/en_com/images/logo_plain.png
+var src=documentgetelementbyid(header)
+src.appendchild(this.img)
+<div id=header></div>
+var audio=new audio (audio_file mp3)
+audio.play 
+jbutton random button
+new jbutton (set random color)
+private final j button option button=
+new j button (change color)
+public fun with graphics(){
+jpanel color panel=new j panel();
+this add (color.panel,border layout.center )
+jpanel button panel=
+new j panel (new flowlayout(flowlayout.center)
+buttonpanel.add(randombutton);
+random.button add actionlistener(new action listener(){
+override 
+public void actionperformed(actionevente){
+random r= newrandom();
+colorpanel.setbackground(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))
+buttonpanel.add(optionbutton);
+optionbutton.add actionlistener(new action listener(){
+override 
+public void actionperformed(actionevente){
+color m=jcolorchooser.showdialog(funwithgraphics.this,))
+if(multi!=null)
+color panel set.background(multi)
+this.add(buttonpanel,borderlayout.south)
+public static void main(string [] args){
+jframe f = new covid strings 3()
+f.set title(covid strings3)
+f.set default closeoperation(jframe.exit_on_close);
+f.setsize
+f.set visible(true);
+f.setsize
+f.set visible(true);
+<label id=lblScore>zero</label>
+var score=zero
+if (mult == x*y)
+[non-concatenated]
+score+=ten
+else:
+score-=ten
+if (mult == x*y)
+[concatenated]
+score+=twenty
+else:
+score-=twenty
+var lblScore= document.getelementbyid(lblscore)
+lbl.score.innerHTML=score
+private j button next button=new jbutton(next)
+nextbutton add.actionlistener(this)
+public void actionperformed(action event e) 
+system.out print.in (next button clicked)
+frame.add(next button);
+frame set visible true
+const object {a:one,b:two,c:three,d:four,e:five,f:six,g:seven,h:eight}
+const keys object keys(team)
+const entries object entries(obj);
+const value object= [team color]
+covid strings3 team list=[black,purple,pink,turquoise,violet,teal,yellow,orange]
+covid strings3 phrase list=[non-concatenated strings a mask is not the shape of an arrowhead,covid rarely wins a vote against vaccines,kelly clarkson is right; christmas isnt canceled Just Big Christmas Gatherings
+A Faucian Bargain Is Better Than A Faustian Bargain 
+The Masters In Fall Is Not The Same As The Fall Of The Masters
+Similarly, The Summer Olympics Doesnt Disappoint Because Of The Fact That Its Held In An Odd Year
+ The UnXplained Should  Have A Covid-Ninteen Specific Special
+Its Pandemic Special Overall Is Still Good
+Concatenated Strings
+Pandemic+Time+Is+Somewhat+Different+Than+Normal+Time
+It+Is+More+Polychronic+Than+You+Think
+Covid+Cough+Cannot+Be+Cured+By+Soda+Alone
+Nothing+About+The+Pandemic+Resembles+The+Holocaust
+It+Is+Also+Inappropriate+To+Not+Vaccinate+The+World+As+Well
+If+You+Dont+You+May+Be+Visited+By+At+Least+Three+More+Variants
+Chinas+Covid+Victory+Musuem+Sounds+Like+North+Koreas+Korean+War+Museum
+There+Are+Plenty+Of+Chinese+Government+Covid+Lies]
+Covid Strings3 Multiple Choice List=[One: A Charging Buffalo B An Arrow C A Mask D Line With A Slope Of Two, Two: A Light Years B Not At All C Somewhat D Similar, Three: A Like Futurama B Polychronic C Monochronic D SNL Like
+,Four: A Influenza B Covid C RSV D SAD, Five: A Dodgeball B Vote C Referendum D Caucus, Six: A You B Trump C Big Gatherings D Bowling Alone, Seven: A Vaccine B Pandemic C Trumps Family Seperation Policy D Ukraine War,
+ Eight: A Wise B Inappropriate C Just D Sensical, Nine: A World Chess Championship B Olympics C Masters D World Cup, Ten: A Four B At Least Three B Eight D Infintely,Eleven:A Lockdown B Trumpian C Faucian D Covid,
+ Twelve: A The Netherlands B The Smithsonians C  Taiwans D North Koreas, Thirteen: A Winter Olympics B X Games C Summer Olmypics D Invictus Games, Fourteen: A Chinese  B Malaysian  C US D Spanish,
+ Fifteen:A Black Death Thirteen Fourty Eight  B Dont Call Covid Kung Flu C Spanish Flu D Covid-Ninteen, Sixteen:A Government Overreach B Defense Against Japanese Killer Ghosts C Protection D Misinformation Campaign,
+Seventeen:A Natural Disaster B Miracle C Graveyard D Pandemic,Eighteen:A Negative Fifteen B Negative One Hundred C Zero D One Hundred Thousand Ninteen:A Trump News B Covid C Russian Interference In Americas Elections D Specific Variants,
+Twenty:A Yes B No.  
+Covid Strings3 Math Problems List=[If There Were Two Hundred Thousand Cases Reported In Wuhan On December Ninteenth And The Threshold For Ordering A Shutdown Is When The Number Of Cases Exceeds Fourty Thousand Would It Shut Down If A Decision Had To Be Made By December Twenty First?,
+If The Rate Was Five And Two Ninths Percent And The Cut For Deciding Whether To Shut Down Is Anything Greater Than Or Equal To Four And A Fifth Percent Should It Shut Down?, After Seven Weeks If The Rate Decreases To Two And Seven Ninths Percent Should The Lockdown Remain In Place?,
+if rates climb to four and a quarter percent should a lockdown be reinstated?,if the rate decreases to four and a fifth percent should the lockdown remain in place?,if oil prices are projected to decline for at least two years and the rate is approximately thirty dollars and seventy three cents per month how much could it decline two years from now?, if the price of oil fell below zero dollars in twenty-twenty and projections of demand coming back say it wont until at least late twenty-twenty one how much would it fall to if the estimated rate per month is five percent?, a+report+claims+disease+x+could+be+twenty+times+deadlier+than+covid+if+gdp+globally+decreased+by+three+percent+how+much+could+it+drop+when+disease+x+hits?,the+us+GDP+Dropped+THREE AND FOUR TENTHS PERCENT+Overall+Given+This+What+Could+It+Drop+To+When+Disease+X+Hits?, If+It+Turns+Out+That+The+Actual+Range+Is+Anywhere+From+Ten+To+Fifteen+Times+What+Would+The+Percent+Be?,On+The+Other+Hand+If+WHO+Projections+Say+That+The+Range+Is+More+Fifteen+To+Twenty+Times+For+Countries+That+Spend+Poorly+If+US+Spending+For+Disease+X+Was+Only+Ten+Percent+What+Is+The+Decrease+In+GDP?,If+It+Turns+Out+That+The+Disease+X+Was+Prevented+From+Being+A+Pandemic+In+The+First+Place+What+Is+The+Effect+On+GDP?,If+It+Ends+Up+Being+That+Disease+X+Was+A+False+Positive+And+There+Is+Nothing+To+Worry+About+Yet+Should+You+Sell+Any+Stock?]
+covid strings3 sentence list=[masks +are +part of a broader protection strategy against covid.,covid+string+games+are+a+new+way+to+help+archive+covid ]
+covid strings3 bonus question=(would+there+need+to+be+a+comma+if+the+previous+sentence+included+the+word+below?)
+string.split   
+arrays.aslist(liststring)
+for (string ss:arr){
+system.outprintin(ss)
+java util.scanner
+java.util
+questions[]questions={
+collections.shuffle(arrays.aslist(questions)
+private string getblanks(int numberofblanks)
+char[] chars = new char[numberofblanks]
+arrays.fill chars 
+return new string chars 
+if math problem== x percent:
+subtract exact from one hundred
+if math problem== margin of error:
+add and subtract exact 
+else:
+remain constant
+userin.select team
+import meta.
+class myclass
+constructor (non-xoncatenated)
+return category
+if category==math problem:
+type answer
+else:
+select from multiple choice
+const myclass=newmyclass(concatenated) 
+return category
+if category==math problem:
+type answer
+else:
+select from multiple choice
+import syntax 
+substring(start,end)
+userin.multiple choice
+if selectedanswer==correct answer:
+return true
+else:
+return false
+return filename
