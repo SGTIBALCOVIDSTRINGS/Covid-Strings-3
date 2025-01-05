e8585765f0a3b8714d02037288705b42e9e1d835
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2025-01-04 21:32:23 -0500

    Update Covid Strings 3 first round.js
    will edit mcirosoft doc.

--- Covid Strings 3 first round.js
+++ Covid Strings 3 first round.js
@@ -1,4 +1,4 @@
-doc. type. HTML
+doc. type.html
 script
 html 
 head
@@ -13,71 +13,58 @@ filename=covidstrings3 first round .js
 createround
 round = document.createelement(div);
 round.classlist.add(covid strings3 first round);
-document addeventlistener(domcontentloaded)
-bracket=document getelementbyiD(bracket)
-creatematchup (teal versus pink)
-matchup= document.createelement(div)
-matchup.covid strings seed list .add(matchup)
-bracketAppend.child(matchup)
-bracket=document getelementbyid(bracket)
-matchup= document.createelement(div)
-create matchup (violet versus turquoise)
-matchup.covid strings seed list .add(matchup)
-bracket append.child(matchup)
-bracket=document getelementbyid(bracket)
-create matchup(yellow versus purple)
-matchup= document.createelement(div)
-matchup.covid strings seed list .add(matchup)
-bracket append.child(matchup)
-matchup= document.createelement(div)
-create matchup(orange versus black)
-matchup.covid strings seed list .add(matchup)
-bracket append.child(matchup)
 return round 
+document addeventlistener(domcontentloaded)
+bracket=document getelementbyid(bracket)
+creatematchup (teal versus pink)
+create matchup (violet versus turquoise)
+create matchup(yellow versus purple)
+create matchup(orange versus black)
+if (event.target.covid strings seed list .contains(matchup)
+return matchup
+else
+add matchup
 teams=[black night riders,purple brave hearts pink covid disease fighters turquoise waves violet relivers teal terminators yellow moxie orange rna messengers]
 java util.scanner
 java.util
 questions[]questions={
-    question1
-    question2 
-    question3
-    question4
-    question5 
-    question6
-    question7
-    question8
-    question9 
-    question10
-    question11
-    question12
-    question13
-    question14 
-    question15 
-    question16 
-    question17 
+question1
+question2 
+question3
+question4
+question5 
+question6
+question7
+question8
+question9 
+question10
+question11
+question12
+question13
+question14 
+question15 
+question16 
+question17 
 collections.shuffle(arrays.aslist(questions)
 private string getblanks(int numberofblanks)
 char[] chars = char[numberofblanks]
 advancingteams=slice(currentteams)
-addeventlistener(clickevent) => {
-declare winner
-if (event.target.covid strings seed list .contains(team)) {
-winner = event.target.textContent;
+addeventlistener(clickevent) 
 result = declarewinner(team1score,team2score);
-if pink score >=fourty:
+if pink score>=fourty:
 console.log(pink covid disease fighters win)
 else:
 console.log(teal terminators win)
-addeventlistener(clickevent) => {
-if (event.target.classlist.contains(team)) {
-winner = event.target.textContent;
+addeventlistener(clickevent)
+if (event.target.classlist.contains(team)) 
+winner = event.target.textcontent;
 result= declare winner(team1 score, team2 score)
 if turquoise score>=fourty:
 console.log(turquoise waves win)
 else:
 console.log(violet relivers win)
-addeventlistener(clickevent) => {
-if (event.target.classlist.contains(team)) {
+addeventlistener(clickevent)
+if (event.target.classlist.contains(team)) 
 winner = event.target.textcontent;
 result= declarewinner(team1 score, team2 score)
 if purple score >=fourty:
