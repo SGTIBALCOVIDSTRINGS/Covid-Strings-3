6f09b8869028272b608081683d64e12ab36fde40
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2025-01-11 21:24:58 -0500

    Update Covid Strings 3 game code.js
    done.

--- Covid Strings 3 game code.js
+++ Covid Strings 3 game code.js
@@ -13,12 +13,12 @@ audio.play
 random button
 new button (set random color)
 private final j button option button=
-new j button (change color)
+new  button (change color)
 public fun with graphics(){
-jpanel color panel=new j panel();
+color panel=new j panel();
 add (panel,center )
 jpanel button panel=
-new j panel (flowlayout.center)
+new  panel (flowlayout.center)
 buttonpanel.add(randombutton);
 random.button add actionlistener(new action listener)
 override 
@@ -73,21 +73,21 @@ keys object keys(team)
 entries object entries(obj);
 value object= [team color]
 covid strings3 team list=[black night riders,purple brave hearts ,pink covid disease fighters ,turquoise waves ,violet relivers ,teal terminators ,yellow moxie ,orange rna messengers]
-covid strings3 phrase list=[non-concatenated strings a mask is not the shape of an arrowhead,covid rarely wins a vote against vaccines,kelly clarkson is right; christmas isnt canceled just big christmas gatherings
-a faucian bargain is better than a faustian bargain 
-the masters in fall is not the same as the fall of the masters
-similarly, the summer olympics doesnt disappoint because of the fact that its held in an odd year
-the unxplained should  have a covid-nineteen specific special
-its pandemic special overall is still good
+covid strings3 phrase list=[non-concatenated strings "a mask is not the shape of an arrowhead,covid rarely wins a vote against vaccines","kelly clarkson is right; christmas isnt canceled just big christmas gatherings"
+"a faucian bargain is better than a faustian bargain" 
+"the masters in fall is not the same as the fall of the masters"
+"similarly, the summer olympics doesnt disappoint because of the fact that its held in an odd year"
+"the unxplained should  have a covid-nineteen specific special"
+"its pandemic special overall is still good"
 concatenated strings
-pandemic+time+is+somewhat+different+than+normal+time
-it+is+more+polychronic+than+you+think
-covid+cough+cannot+be+cured+by+soda+alone
-nothing+about+the+pandemic+resembles+the+holocaust
-it+is+also+inappropriate+to+not+vaccinate+the+world+as+well
-if+you+dont+you+may+be+visited+by+at+least+three+more+variants
-chinas+covid+victory+musuem+sounds+like+north+koreas+korean+war+museum
-there+are+plenty+of+chinese+government+covid+lies]
+"pandemic"+"time"+"is"+"somewhat"+"different"+"than"+"normal"+"time"
+"it"+"is"+"more"+"polychronic"+"than"+"you"+"think"
+"covid"+"cough"+"cannot"+"be"+"cured"+"by"+"soda"+"alone"
+"nothing"+"about"+"the"+"pandemic"+"resembles"+"the"+"holocaust"
+"it"+"is"+"also"+"inappropriate"+"to"+"not"+"vaccinate"+"the"+"world"+"as"+"well"
+"if"+"you"+"dont"+"you"+"may"+"be"+"visited"+"by"+"at"+"least"+"three"+"more"+"variants"
+"chinas"+"covid"+"victory"+"musuem"+"sounds"+"like"+"north"+"koreas"+"korean"+"war"+"museum"
+"there"+"are"+"plenty"+"of"+"chinese"+"government"+"covid"+"lies"]
 covid strings3 multiple choice list=[1:a charging buffalo b an arrow c a mask d line with a slope of two, 2: a light years b not at all c somewhat d similar, 3: a like futurama b polychronic c monochronic d snl like
 ,4: a influenza b covid c rsv d sad, 5: a dodgeball b vote c referendum d caucus,6: a you b trump c big gatherings d bowling alone, 7: a vaccine b pandemic c trumps family seperation policy d ukraine war,
  8: a wise b inappropriate c just d sensical, 9: a world chess championship b olympics c masters d world cup, 10: a four b at least three c eight d infintely,11:a lockdown b trumpian c faucian d covid,
@@ -95,11 +95,12 @@ covid strings3 multiple choice list=[1:a charging buffalo b an arrow c a mask d 
  15:a black death thirteen fourty eight b dont call covid kung flu c spanish flu d covid-nineteen, 16:a government overreach b defense against japanese killer ghosts c protection d misinformation campaign,
 17:a natural disaster b miracle c graveyard d pandemic,18:a negative fifteen b negative one hundred c zero d one hundred thousand 19:a trump news b covid c russian interference in americas elections d specific variants,
 20:a yes b no.  
-covidstrings3 math problems list=[if there were two hundred thousand cases reported in wuhan on december ninteenth and the threshold for ordering a shutdown is when the number of cases exceeds fourty thousand would it shut down if a decision had to be made by december twenty first?,
-if the rate was five and two ninths percent and the cut for deciding whether to shut down is anything greater than or equal to four and a fifth percent should it shut down?, after seven weeks if the rate decreases to two and seven ninths percent should the lockdown remain in place?,
-if rates climb to four and a quarter percent should a lockdown be reinstated?,if the rate decreases to four and a fifth percent should the lockdown remain in place?,if oil prices are projected to decline for at least two years and the rate is approximately thirty dollars and seventy three cents per month how much could it decline two years from now?, if the price of oil fell below zero dollars in twenty-twenty and projections of demand coming back say it wont until at least late twenty-twenty one how much would it fall to if the estimated rate per month is five percent?, a+report+claims+disease+x+could+be+twenty+times+deadlier+than+covid+if+gdp+globally+decreased+by+three+percent+how+much+could+it+drop+when+disease+x+hits?,the+us+gdp+dropped+three and four tenths percent+overall+given+this+what+could+it+drop+to+when+disease+x+hits?,if+it+turns+out+that+the+actual+range+is+anywhere+from+ten+to+fifteen+times+what+would+the+percent+be?,on+the+other+hand+if+who+projections+say+that+the+range+is+more+fifteen+to+twenty+times+for+countries+that+spend+poorly+if+us+spending+for+disease+x+was+only+ten+percent+what+is+the+decrease+in+gdp?,if+it+turns+out+that+the+disease+x+was+prevented+from+being+a+pandemic+in+the+first+place+what+is+the+effect+on+gdp?,if+it+ends+up+being+that+disease+x+was+a+false+positive+and+there+is+nothing+to+worry+about+yet+should+you+sell+any+stock?]
-covid strings3 sentence list=[masks +are +part of a broader protection strategy against covid.,covid+string+games+are+a+new+way+to+help+archive+covid ]
-covid strings3 bonus question=(would+there+need+to+be+a+comma+if+the+previous+sentence+included+the+word+below?)
+covidstrings3 math problems list=[ non concatenated "if there were two hundred thousand cases reported in wuhan on december ninteenth and the threshold for ordering a shutdown is when the number of cases exceeds fourty thousand would it shut down if a decision had to be made by december twenty first"?,
+"if the rate was five and two ninths percent and the cut for deciding whether to shut down is anything greater than or equal to four and a fifth percent should it shut down"?, "after seven weeks if the rate decreases to two and seven ninths percent should the lockdown remain in place?",
+"if rates climb to four and a quarter percent should a lockdown be reinstated"?,"if the rate decreases to four and a fifth percent should the lockdown remain in place"?,"if oil prices are projected to decline for at least two years and the rate is approximately thirty dollars and seventy three cents per month how much could it decline two years from now"?, "if the price of oil fell below zero dollars in twenty-twenty and projections of demand coming back say it wont until at least late twenty-twenty one how much would it fall to if the estimated rate per month is five percent"?, 
+ concatenated "a"+"report"+"claims"+"disease"+"x"+"could"+"be"+"twenty"+"times"+"deadlier"+"than"+"covid"+"if"+"gdp"+"globally"+"decreased"+"by"+"three"+"percent"+"how"+"much"+"could"+"it"+"drop"+"when"+"disease"+"x"+"hits"?,"the"+"us"+"gdp"+"dropped"+"three and four tenths percent"+"overall"+"given"+"this"+"what"+"could"+"it"+"drop"+"to"+"when"+"disease"+"x"+"hits"?,"if"+"it"+"turns"+"out"+"that"+"the"+"actual"+"range"+"is"+"anywhere"+"from"+"ten"+"to"+"fifteen"+"times"+"what"+"would"+"the"+"percent"+"be"?,"on"+"the"+"other"+"hand"+"if"+"who"+"projections"+"say"+"that"+"the"+"range"+"is"+"more"+"fifteen"+"to"+"twenty"+"times"+"for"+"countries"+"that"+"spend"+"poorly"+"if"+"us"+"spending"+"for"+"disease"+"x"+"was"+"only"+"ten"+"percent"+"what"+"is"+"the"+"decrease"+"in"+"gdp"?,"if"+"it"+"turns"+"out"+"that"+"the"+"disease"+"x"+"was"+"prevented"+"from"+"being"+"a"+"pandemic"+"in"+"the"+"first"+"place"+"what"+"is"+"the"+"effect"+"on"+"gdp"?,"if"+"it"+"ends"+"up"+"being"+"that"+"disease"+"x"+"was"+"a"+"false"+"positive"+"and"+"there"+"is"+"nothing"+"to"+"worry"+"about"+"yet"+"should"+"you"+"sell"+"any"+"stock"?]
+covid strings3 sentence list=["masks"+"are"+"part"+"of"+"a"+"broader"+"protection"+"strategy"+"against"+"covid","covid"+"string"+"games"+"are"+"a"+"new"+"way"+"to"+"help"+"archive"+"covid"]
+covid strings3 bonus question=("would"+"there"+"need"+"to"+"be"+"a"+"comma"+"if"+"the"+"previous"+"sentence"+"included"+"the"+"word"+"below"?)
 string.split   
 arrays.aslist(liststring)
 for (string ss:arr){
