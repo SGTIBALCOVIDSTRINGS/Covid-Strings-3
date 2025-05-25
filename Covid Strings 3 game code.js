ec02f91b35b0bae0d19e157b9705e40f8a38567b
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2025-05-25 12:28:09 -0400

    Update Covid Strings 3 game code.js
    will add changes later.

--- Covid Strings 3 game code.js
+++ Covid Strings 3 game code.js
@@ -1,29 +1,60 @@
 doc type html
+html. lang.en
 script
+header=document create.element (header)
+header.style.backgroundcolor=#4caf50
+header.style.color=’white’
+header.style.padding=’10px’
+header.style.textalign=’center’
+header.style.text.fontsize=’24px’
+header.textcontent=’welcome to covid strings3’
+document appendchild(header)
+<header.html>
+heading variables 
+the heading numbers that determine where each heading and subheading go 
+heading1=game code overview
+heading2=background functions
+heading3=game code part1
+heading4=phrases
+heading5=multiple choice problems
+heading6=math problems
+heading7=sentences
+heading8=game code part2 
+heading9=answer checker
+heading10=winner checker
+subheading1=polling questions
+subheading2=bonus question
+subheading3= answer scanner
 this.img=document.createelement(covid)
 img.src=http://www.google.com/intl/en_com/images/logo_plain.png
 src=documentgetelementbyid(header)
 src.appendchild(this.img)
 filename=covid strings3 .js
 src.appendchild(this.img)
+<header.html>
+game code overview
+an outline of the game code for covid strings3
 java.awt graphics 
 div id=header/div
 audio=new audio (audio_file mp3)
 audio.play 
 random button
 new button (set random color)
-private final j button option button=
+option button
 new  button (change color)
-public fun with graphics(){
+public fun with graphics()
 color panel=new j panel();
 add (panel,center )
-jpanel button panel=
+button panel=
 new  panel (flowlayout.center)
 buttonpanel.add(randombutton);
 random.button add actionlistener(new action listener)
 override 
-public actionperformed(actionevente){
+public actionperformed(actionevente)
 random r= newrandom();
+<header.html>
+background functions
+the functions that lead into the background that are the first things the user will experience
 colorpanel.setbackground(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen),)(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))(rnextint(thirteen)))
 num teams
 total players= players per team
@@ -72,8 +103,35 @@ object {a:one,b:two,c:three,d:four,e:five,f:six,g:seven,h:eight}
 keys object keys(team)
 entries object entries(obj);
 value object= [team color]
+<header.html>
+game code part1
+part1 of game code for covid strings3(value object[team-color]-desired color green)
 covid strings3 team list=[black night riders,purple brave hearts ,pink covid disease fighters ,turquoise waves ,violet relivers ,teal terminators ,yellow moxie ,orange rna messengers]
-covid strings3 phrase list=[non-concatenated strings "a mask is not the shape of an arrowhead,covid rarely wins a vote against vaccines","kelly clarkson is right; christmas isnt canceled just big christmas gatherings"
+string manager{non-concatenated}
+insert string (entity,string)
+entity.element.text=string
+string manager insert string
+string manager{concatenated}
+insert string (entity,string)
+entity.element.text=string
+string manager insert string
+string=””
+for string in covid strings3 phrase list 
+select characters
+edit diffuse property
+desired color=green
+for string in covid strings3 math problems list 
+select characters
+edit diffuse property
+desired color=green
+for string in covid strings3 sentence list 
+select characters
+edit diffuse property
+desired color=green
+<header.html>
+phrases
+phrases that involve both non-concatenated and concatenated strings 
+ covid strings3 phrase list=[non-concatenated strings "a mask is not the shape of an arrowhead,covid rarely wins a vote against vaccines","kelly clarkson is right; christmas isnt canceled just big christmas gatherings"
 "a faucian bargain is better than a faustian bargain" 
 "the masters in fall is not the same as the fall of the masters"
 "similarly, the summer olympics doesnt disappoint because of the fact that its held in an odd year"
@@ -88,6 +146,9 @@ concatenated strings
 "if"+"you"+"dont"+"you"+"may"+"be"+"visited"+"by"+"at"+"least"+"three"+"more"+"variants"
 "chinas"+"covid"+"victory"+"musuem"+"sounds"+"like"+"north"+"koreas"+"korean"+"war"+"museum"
 "there"+"are"+"plenty"+"of"+"chinese"+"government"+"covid"+"lies"]
+<header.html>
+multiple choice questions
+multiple choice questions and answers that will appear in covid strings3 
 covid strings3 multiple choice list=[1:a charging buffalo b an arrow c a mask d line with a slope of two, 2: a light years b not at all c somewhat d similar, 3: a like futurama b polychronic c monochronic d snl like
 ,4: a influenza b covid c rsv d sad, 5: a dodgeball b vote c referendum d caucus,6: a you b trump c big gatherings d bowling alone, 7: a vaccine b pandemic c trumps family seperation policy d ukraine war,
  8: a wise b inappropriate c just d sensical, 9: a world chess championship b olympics c masters d world cup, 10: a four b at least three c eight d infintely,11:a lockdown b trumpian c faucian d covid,
@@ -95,12 +156,52 @@ covid strings3 multiple choice list=[1:a charging buffalo b an arrow c a mask d 
  15:a black death thirteen fourty eight b dont call covid kung flu c spanish flu d covid-nineteen, 16:a government overreach b defense against japanese killer ghosts c protection d misinformation campaign,
 17:a natural disaster b miracle c graveyard d pandemic,18:a negative fifteen b negative one hundred c zero d one hundred thousand 19:a trump news b covid c russian interference in americas elections d specific variants,
 20:a yes b no.  
-covidstrings3 math problems list=[ non concatenated "if there were two hundred thousand cases reported in wuhan on december ninteenth and the threshold for ordering a shutdown is when the number of cases exceeds fourty thousand would it shut down if a decision had to be made by december twenty first"?,
+<header.html>
+math problems
+covid related math problems that will appear in covid strings3
+covidstrings3 math problems list=[ non concatenated strings  "if there were two hundred thousand cases reported in wuhan on december ninteenth and the threshold for ordering a shutdown is when the number of cases exceeds fourty thousand would it shut down if a decision had to be made by december twenty first"?,
 "if the rate was five and two ninths percent and the cut for deciding whether to shut down is anything greater than or equal to four and a fifth percent should it shut down"?, "after seven weeks if the rate decreases to two and seven ninths percent should the lockdown remain in place?",
 "if rates climb to four and a quarter percent should a lockdown be reinstated"?,"if the rate decreases to four and a fifth percent should the lockdown remain in place"?,"if oil prices are projected to decline for at least two years and the rate is approximately thirty dollars and seventy three cents per month how much could it decline two years from now"?, "if the price of oil fell below zero dollars in twenty-twenty and projections of demand coming back say it wont until at least late twenty-twenty one how much would it fall to if the estimated rate per month is five percent"?, 
- concatenated "a"+"report"+"claims"+"disease"+"x"+"could"+"be"+"twenty"+"times"+"deadlier"+"than"+"covid"+"if"+"gdp"+"globally"+"decreased"+"by"+"three"+"percent"+"how"+"much"+"could"+"it"+"drop"+"when"+"disease"+"x"+"hits"?,"the"+"us"+"gdp"+"dropped"+"three and four tenths percent"+"overall"+"given"+"this"+"what"+"could"+"it"+"drop"+"to"+"when"+"disease"+"x"+"hits"?,"if"+"it"+"turns"+"out"+"that"+"the"+"actual"+"range"+"is"+"anywhere"+"from"+"ten"+"to"+"fifteen"+"times"+"what"+"would"+"the"+"percent"+"be"?,"on"+"the"+"other"+"hand"+"if"+"who"+"projections"+"say"+"that"+"the"+"range"+"is"+"more"+"fifteen"+"to"+"twenty"+"times"+"for"+"countries"+"that"+"spend"+"poorly"+"if"+"us"+"spending"+"for"+"disease"+"x"+"was"+"only"+"ten"+"percent"+"what"+"is"+"the"+"decrease"+"in"+"gdp"?,"if"+"it"+"turns"+"out"+"that"+"the"+"disease"+"x"+"was"+"prevented"+"from"+"being"+"a"+"pandemic"+"in"+"the"+"first"+"place"+"what"+"is"+"the"+"effect"+"on"+"gdp"?,"if"+"it"+"ends"+"up"+"being"+"that"+"disease"+"x"+"was"+"a"+"false"+"positive"+"and"+"there"+"is"+"nothing"+"to"+"worry"+"about"+"yet"+"should"+"you"+"sell"+"any"+"stock"?]
+concatenated strings  "a"+"report"+"claims"+"disease"+"x"+"could"+"be"+"twenty"+"times"+"deadlier"+"than"+"covid"+"if"+"gdp"+"globally"+"decreased"+"by"+"three"+"percent"+"how"+"much"+"could"+"it"+"drop"+"when"+"disease"+"x"+"hits"?,"the"+"us"+"gdp"+"dropped"+"three and four tenths percent"+"overall"+"given"+"this"+"what"+"could"+"it"+"drop"+"to"+"when"+"disease"+"x"+"hits"?,"if"+"it"+"turns"+"out"+"that"+"the"+"actual"+"range"+"is"+"anywhere"+"from"+"ten"+"to"+"fifteen"+"times"+"what"+"would"+"the"+"percent"+"be"?,"on"+"the"+"other"+"hand"+"if"+"who"+"projections"+"say"+"that"+"the"+"range"+"is"+"more"+"fifteen"+"to"+"twenty"+"times"+"for"+"countries"+"that"+"spend"+"poorly"+"if"+"us"+"spending"+"for"+"disease"+"x"+"was"+"only"+"ten"+"percent"+"what"+"is"+"the"+"decrease"+"in"+"gdp"?,"if"+"it"+"turns"+"out"+"that"+"the"+"disease"+"x"+"was"+"prevented"+"from"+"being"+"a"+"pandemic"+"in"+"the"+"first"+"place"+"what"+"is"+"the"+"effect"+"on"+"gdp"?,"if"+"it"+"ends"+"up"+"being"+"that"+"disease"+"x"+"was"+"a"+"false"+"positive"+"and"+"there"+"is"+"nothing"+"to"+"worry"+"about"+"yet"+"should"+"you"+"sell"+"any"+"stock"?]
+public static main(string []args)
+int number
+number=number*2
+system.out print in (“the number is“ +number)
+<subheading.html>
+polling problems
+math problems that involve the use of polls
+polling function (exact,number)
+console log (exact:${exact}, number:${number},
+if polling function log== (exact,number)
+return true
+else:
+return false
+if polling function log==(number,exact)
+return false
+else:
+return true  
+for math problems in covid strings math problem list 
+if math problem==x percent:
+subtract exact from one hundred
+if math problem==margin of error:
+add and subtract exact number 
+else:
+remain 
+<header.html>
+sentences
+covid related sentences that will appear in covid strings3
 covid strings3 sentence list=["masks"+"are"+"part"+"of"+"a"+"broader"+"protection"+"strategy"+"against"+"covid","covid"+"string"+"games"+"are"+"a"+"new"+"way"+"to"+"help"+"archive"+"covid"]
+public static main(string []args)
+int number
+number=number*2
+system.out print in (“the number is“ +number)
+<subheading.html>
+bonus question
+the extra question or questions that appear around the end of the game
 covid strings3 bonus question=("would"+"there"+"need"+"to"+"be"+"a"+"comma"+"if"+"the"+"previous"+"sentence"+"included"+"the"+"word"+"below"?)
+<header.html>
+game code part2
+the rest of the game code excluding answer and winner checker
 string.split   
 arrays.aslist(liststring)
 for (string ss:arr){
@@ -136,11 +237,34 @@ else:
 select from multiple choice
 importsyntax 
 substring(start,end)
-userin.multiple choice
+<header.html>
+answer checker
+the feature within covid strings that allows the software to check whether a users answer is correct
+for multiple choice questions in covid strings 
 if selectedanswer==correct answer:
-log true
+return true
 else:
-log false
+return false
+if selectedanswer==incorrect answer:
+return false
+else:
+return true
+for math problems in covid strings
+if typed answer ==correct
+return true
+else:
+return false
+if typed answer== not correct
+return false
+else
+return true
+public static main(string [] args)
+int number
+number=number*2
+system.out print in (“the number is“ +number)
+<subheading.html>
+answer scanner
+the feature within the covid strings answer checker that allows for confirmation of the user 
 answer checker
 scanner=new scanner(system.in)
 string correct answer
@@ -150,6 +274,9 @@ system.out.print.in(correct)
 else:
 system.out.printin(incorrect)
 scanner.close
+<header.html>
+winner checker
+the feature within covid strings that allows for the player to know if their team won 
 check winner:
 if winner==true:
 log(the winner is)
@@ -158,5 +285,9 @@ log(no winner yet)
 if users team wins covid strings3 
 console log users team wins covid strings3 
 else
+remain
+if users team loses covid strings3 
 return game over 
+else
+remain
 return filename
